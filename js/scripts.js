a4fd2adf431b4e5c63af7d0af9ffe9bb083e5b1a
Gengur123 <meleearthur@gmail.com>
2021-06-18 10:06:11 -0700

    Added Jquery test inputs and compared its value

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,16 @@
 $(document).ready(function() {
   $("form#Q1").submit(function(){
     alert("You clicked a radio")
+  });
 
-  })
+  $('input[name="size"]').click(function(){
+    let size = $('input[name="size"]:checked').val()
+    if (size === "small") {
+      alert("small pizza")
+    }else if (size === "medium") {
+      alert("medium pizza")
+    }else {
+      alert("large pizza")
+    }
+  });
 });
