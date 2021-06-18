9399c28028ba726bd75acd16732b363fd9d0f67d
Gengur123 <meleearthur@gmail.com>
2021-06-18 13:26:20 -0700

    Added hide and show for all the form pictures

--- js/scripts.js
+++ js/scripts.js
@@ -20,11 +20,49 @@ $(document).ready(function () {
       $("#mediumpizza").show();
       $("#smallpizza").hide();
       $("#largepizza").hide();
-      
+
     } else {
       $("#largepizza").show();
       $("#smallpizza").hide();
       $("#mediumpizza").hide();
+    }
+  });
+
+  $('input[name="crust"]').click(function () {
+    let crust = $('input[name="crust"]:checked').val()
+    if (crust === "original") {
+      $("#panpizza").show();
+      $("#handpizza").hide();
+      $("#thinpizza").hide();
+
+    } else if (crust === "tossed") {
+      $("#handpizza").show();
+      $("#panpizza").hide();
+      $("#thinpizza").hide();
+      
+    } else {
+      $("#thinpizza").show();
+      $("#handpizza").hide();
+      $("#panpizza").hide();
+    }
+  });
+
+  $('input[name="topping"]').click(function () {
+    let topping = $('input[name="topping"]:checked').val()
+    if (topping === "pepperoni") {
+      $("#pepperonipizza").show();
+      $("#meatpizza").hide();
+      $("#supremepizza").hide();
+
+    } else if (topping === "meat") {
+      $("#meatpizza").show();
+      $("#pepperonipizza").hide();
+      $("#supremepizza").hide();
+      
+    } else {
+      $("#supremepizza").show();
+      $("#pepperonipizza").hide();
+      $("#meatpizza").hide();
     }
   });
 });
