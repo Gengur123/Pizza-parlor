fe5ec6512d9b837e333f835f788585601c292800
Gengur123 <meleearthur@gmail.com>
2021-06-18 11:47:41 -0700

    added 9 photos and completed hiding photos and showing them when clicking a option

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,30 @@
-$(document).ready(function() {
+$(document).ready(function () {
   function pizza() {
     this.size = "d";
     this.crust = "d";
     this.topping = "d";
   }
 
-  $("form#Q1").submit(function(){
+  $("form#Q1").submit(function () {
     alert("You clicked a radio")
   });
 
-  $('input[name="size"]').click(function(){
+  $('input[name="size"]').click(function () {
     let size = $('input[name="size"]:checked').val()
     if (size === "small") {
-      alert("small pizza")
-    }else if (size === "medium") {
-      alert("medium pizza")
-    }else {
-      alert("large pizza")
+      $("#smallpizza").show();
+      $("#mediumpizza").hide();
+      $("#largepizza").hide();
+
+    } else if (size === "medium") {
+      $("#mediumpizza").show();
+      $("#smallpizza").hide();
+      $("#largepizza").hide();
+      
+    } else {
+      $("#largepizza").show();
+      $("#smallpizza").hide();
+      $("#mediumpizza").hide();
     }
   });
 });
